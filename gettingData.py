91cd58641c38562f5e37f885c4a6ca17c6641bcf
Justin Pang <jfpthree60@gmail.com>
2022-08-30 12:21:58 -0400

    macro and micro name search implemented - user enters player name, then chooses adjacent index - able to print player name and its stats beside eachother

--- gettingData.py
+++ gettingData.py
@@ -1,17 +1,37 @@
 import csv
+import os
 
 Name = input("Please provide name: ")
+directory = 'posted stats'
 
-with open('posted stats/BlueJays 23,08,2022 12,18,21.csv') as csvfile:
+#get specific name of player from newest file
+latestFile = os.listdir(directory)[-1]
+print(latestFile)
+
+#ask user for macroscopic search
+with open(f"posted stats/{latestFile}") as csvfile:
     reader = csv.DictReader(csvfile)
-    #ask user for macroscopic search
     for index, row in enumerate(reader):
         if Name.lower() in row['Name'].lower():
             print("Here are the names: ",row['Name'])
             print("\nindex: ", index)
         else:
             print("No")
-    #ask user for specific search
-    numPlayer = input("Provide index: ")
+
+#ask user for specific search  
+with open(f"posted stats/{latestFile}") as csvfile:
+    numPlayer = int(input("Provide index: "))
+    reader = csv.DictReader(csvfile)
+    for index, row in enumerate(reader):
+        if index == numPlayer:
+            playerName = row['Name']
+
+
+for filename in os.listdir(directory):
+    f = open(f"posted stats/{filename}", "r")
+    reader = csv.DictReader(f)
+    for index, row in enumerate(reader):
+        if playerName in row['Name']:
+            print(row['Name'], "and", row['Avg'])
 
 
